4b30e9e5f3827157f4cb63d26a83fb2a5d7419b4
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2018-10-02 20:57:50 -0300

    First commit

---
+++ script/script.js
@@ -0,0 +1,32 @@
+const button = document.querySelector(".transacao__button");
+
+button.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const tabela = document.querySelector(".extrato__table");
+
+    const inputNome = document.querySelector("#transacaoInputName");
+    const inputValor = document.querySelector("#transacaoInputMoney");
+    const inputData = document.querySelector("#transacaoInputDate");
+
+    const linha = document.createElement("tr");
+
+    const colunaNome = document.createElement("td");
+    const colunaNomeConteudo = document.createTextNode(inputNome.value);
+    colunaNome.appendChild(colunaNomeConteudo);
+
+    const colunaValor = document.createElement("td");
+    const colunaValorConteudo = document.createTextNode(inputValor.value);
+    colunaValor.appendChild(colunaValorConteudo);
+
+    const colunaData = document.createElement("td");
+    const colunaDataConteudo = document.createTextNode(inputData.value);
+    colunaData.appendChild(colunaDataConteudo);
+
+    linha.appendChild(colunaNome);
+    linha.appendChild(colunaValor);
+    linha.appendChild(colunaData);
+
+    tabela.appendChild(linha);
+
+})
