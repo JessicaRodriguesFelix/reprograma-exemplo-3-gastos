305e44033b4df9355b22d06c433ef669f01b2b33
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2018-10-02 22:13:42 -0300

    criação de condicinal

--- script/script.js
+++ script/script.js
@@ -1,32 +1,42 @@
 const button = document.querySelector(".transacao__button");
 
-button.addEventListener('click', function(event){
+button.addEventListener('click', function (event) {
     event.preventDefault();
 
     const tabela = document.querySelector(".extrato__table");
-
     const inputNome = document.querySelector("#transacaoInputName");
     const inputValor = document.querySelector("#transacaoInputMoney");
     const inputData = document.querySelector("#transacaoInputDate");
 
-    const linha = document.createElement("tr");
+    if (inputNome.value == "") {
+        alert("Campo nome Vazio!");
+    }
+    else if (inputValor.value == "") {
+        alert("Campo Valor Vazio");
+    }
+    else if (inputData.value == "") {
+        alert("Campo Data Vazio");
+    }
+    else {
+        const linha = document.createElement("tr");
 
-    const colunaNome = document.createElement("td");
-    const colunaNomeConteudo = document.createTextNode(inputNome.value);
-    colunaNome.appendChild(colunaNomeConteudo);
+        const colunaNome = document.createElement("td");
+        const colunaNomeConteudo = document.createTextNode(inputNome.value);
+        colunaNome.appendChild(colunaNomeConteudo);
 
-    const colunaValor = document.createElement("td");
-    const colunaValorConteudo = document.createTextNode(inputValor.value);
-    colunaValor.appendChild(colunaValorConteudo);
+        const colunaValor = document.createElement("td");
+        const colunaValorConteudo = document.createTextNode(inputValor.value);
+        colunaValor.appendChild(colunaValorConteudo);
 
-    const colunaData = document.createElement("td");
-    const colunaDataConteudo = document.createTextNode(inputData.value);
-    colunaData.appendChild(colunaDataConteudo);
+        const colunaData = document.createElement("td");
+        const colunaDataConteudo = document.createTextNode(inputData.value);
+        colunaData.appendChild(colunaDataConteudo);
 
-    linha.appendChild(colunaNome);
-    linha.appendChild(colunaValor);
-    linha.appendChild(colunaData);
+        linha.appendChild(colunaNome);
+        linha.appendChild(colunaValor);
+        linha.appendChild(colunaData);
 
-    tabela.appendChild(linha);
+        tabela.appendChild(linha);
+    }
 
 })
